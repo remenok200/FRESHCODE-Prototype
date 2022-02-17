aef8e449c94d1a704bbb2e11410d092c6cc0359f
remenok200 <remenok9@gmail.com>
2022-02-17 17:29:24 +0200

    fix

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function Car(
   model,
   color,
   releaseYear,
-  speed,
   maxSpeed,
   fuelConsumption,
   volume
@@ -15,7 +14,6 @@ function Car(
     typeof model !== "string" ||
     typeof color !== "string" ||
     typeof releaseYear !== "string" ||
-    typeof speed !== "number" ||
     typeof maxSpeed !== "number" ||
     typeof fuelConsumption !== "number" ||
     typeof volume !== "number"
@@ -33,9 +31,7 @@ function Car(
     releaseYear === "" ||
     releaseYear === undefined ||
     color === "" ||
-    speed === undefined ||
     //isNaN(speed) === true ||
-    speed < 0 ||
     maxSpeed === undefined ||
     //isNaN(maxSpeed) === true ||
     maxSpeed < 0 ||
@@ -43,24 +39,24 @@ function Car(
     //isNaN(fuelConsumption) === true ||
     fuelConsumption < 0 ||
     volume === undefined ||
-    isNaN(volume + fuelConsumption + maxSpeed + speed) === true ||
+    isNaN(volume + fuelConsumption + maxSpeed) === true ||
     volume < 0
   ) {
     throw new Error(
       "Поля не могут быть пустыми или не определенными, кроме того, числа не могут быть отрицательными. Проверяйте, что вводите!"
     );
   }
-  if (speed > maxSpeed) {
-    throw new RangeError(
-      "Скорость ну никак не может быть больше максимальной!"
-    );
-  }
+  // if (speed > maxSpeed) {
+  //   throw new RangeError(
+  //     "Скорость ну никак не может быть больше максимальной!"
+  //   );
+  // }
 
   this.manufacturer = manufacturer;
   this.model = model;
   this.color = color;
   this.releaseYear = releaseYear;
-  this.speed = speed;
+  this.speed = 0;
   this.maxSpeed = maxSpeed;
   this.fuelConsumption = fuelConsumption;
   this.volume = volume;
@@ -68,10 +64,24 @@ function Car(
 
 function CarPrototype() {
   this.accelerate = function (number) {
-    return (this.speed += number); // без return тоже работает; нужен ли он?
+    if (number + this.speed > this.maxSpeed) {
+      throw new RangeError(
+        `Скорость ну никак не может быть больше максимальной! Предлагаем Вам снизить скорость до максимально допустимой, то есть на ${
+          this.speed + number - this.maxSpeed
+        }`
+      );
+    }
+    return (this.speed += number);
   };
 
   this.deAccelerate = function (number) {
+    if (this.speed - number < 0) {
+      throw new RangeError(
+        `Машина не может ехать с отрицательной скоростью! Предлагаем Вам уменьшить число торможения таким образом, чтобы скорость вышла в 0, то есть на ${Math.abs(
+          this.speed - number
+        )}`
+      );
+    }
     return (this.speed -= number);
   };
 
@@ -82,9 +92,9 @@ function CarPrototype() {
 
 Car.prototype = new CarPrototype();
 
-const toyota = new Car("Toyota", "Prius", "white", "2018", 20, 160, 1, 60);
-const lexus = new Car("Lexus", "LX-15", "yellow", "2019", 38.5, 195, 2, 69);
-const bmw = new Car("BMW", "X5", "black", "2016", 70, 250, 3, 150);
+const toyota = new Car("Toyota", "Prius", "white", "2018", 160, 1, 60);
+const lexus = new Car("Lexus", "LX-15", "yellow", "2019", 195, 2, 69);
+const bmw = new Car("BMW", "X5", "black", "2016", 250, 3, 150);
 
 console.log("Состояние объектов до изменений:");
 console.log(toyota);
@@ -104,9 +114,9 @@ console.log(
 console.log(
   `Скорость объектов до ее изменения (торможение): ${toyota.speed} ${lexus.speed} ${bmw.speed}`
 );
-toyota.deAccelerate(16);
-lexus.deAccelerate(30);
-bmw.deAccelerate(40);
+toyota.deAccelerate(4);
+lexus.deAccelerate(8);
+bmw.deAccelerate(12);
 console.log(
   `Скорость объектов после ее изменения (торможение): ${toyota.speed} ${lexus.speed} ${bmw.speed}`
 );
@@ -126,7 +136,7 @@ console.log(toyota);
 console.log(lexus);
 console.log(bmw);
 
-const testMachine4 = new Car("Toyota", "Prius", "white", "2018", 300, 160, 1, 60); // вернет ошибку
-const testMachine3 = new Car("", "LX-15", "yellow", "2019", 38.5, 195, 2, 69); // вернет ошибку
-const testMachine2 = new Car("BMW", "X5", "black", "2016", 70, 250, 3, -5); // вернет ошибку
-const testMachine1 = new Car(100, "Prius", "white", "2018", 20, 160, 1, 60); // вернет ошибку
+const testMachine4 = new Car("Toyota", "Prius", "white", "2018", 160, 1, 60); // вернет ошибку
+const testMachine3 = new Car("", "LX-15", "yellow", "2019", 195, 2, 69); // вернет ошибку
+const testMachine2 = new Car("BMW", "X5", "black", "2016", 250, 3, -5); // вернет ошибку
+const testMachine1 = new Car(100, "Prius", "white", "2018", 160, 1, 60); // вернет ошибку
